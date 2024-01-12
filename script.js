89fb4325685e1dd7689957d8be5c02d09fc4b9e9
FlorentM <f.madouri@free.fr>
2024-01-12 10:35:04 +0100

    Tâche terminée

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ var drop = document.getElementById('dropper');
 drop.addEventListener('dragover', function (event){
     event.preventDefault();
 })
+
+/* Quand drag au dessus de drop */
+drop.addEventListener('dragenter', function (event){
+    drop.style.backgroundColor = 'yellow';
+})
+
+/* Quand drag sort de la drop zone */
+drop.addEventListener('dragleave', function (event){
+    drop.style.backgroundColor = 'pink';
+})
+
+
+
+/* Add Data de drag à drop */
+drag.addEventListener('dragstart', function (event){
+    event.dataTransfer.setData('text/plain', drag.innerHTML);
+})
+
+drop.addEventListener('drop', (event) =>{
+    const data = event.dataTransfer.getData('text/plain');
+    drop.innerHTML = ` ${data}  ${drop.innerHTML}`;
+    drop.style.backgroundColor = 'pink';
+} )
+
